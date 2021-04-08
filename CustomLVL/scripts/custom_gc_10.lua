2521d51d06be2f6c74a6547c4544689fa6c75697
GT-Anakin <Battlefrontler@t-online.de>
2021-04-08 15:07:17 +0200

    recovered custom_gc_10.lua

--- CustomLVL/scripts/custom_gc_10.lua
+++ CustomLVL/scripts/custom_gc_10.lua
@@ -1,3 +1,24 @@
 ------------------------------------------------------------------
 -- uop recovered source
 ------------------------------------------------------------------
+
+print("custom_gc_10: Entered")
+
+local i = 10
+
+while true do
+	
+	i = i + 1;
+	local scriptName = "custom_gc_" .. tostring(i)
+	
+	if ScriptCB_IsFileExist(scriptName .. ".lvl") == 0 then
+		print("custom_gc_10: No " .. scriptName .. ".lvl.  Will stop searching for any more cGC scripts.")
+		break;
+	else
+		print("custom_gc_10: Found " .. scriptName .. ".lvl")
+		ReadDataFile(scriptName .. ".lvl")
+		ScriptCB_DoFile(scriptName)
+	end
+end
+
+print("custom_gc_10: Exited")
