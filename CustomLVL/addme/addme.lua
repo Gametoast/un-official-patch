ed474707f419f315e8664be53443eeedd55263cb
GT-Anakin <Battlefrontler@t-online.de>
2021-05-03 11:01:46 +0200

    debug mod installed to addme.lua

--- CustomLVL/addme/addme.lua
+++ CustomLVL/addme/addme.lua
@@ -3,7 +3,9 @@
 -- Original By Zerted
 
 
-print("Mission Checker: Entered addme")
+print("Unofficial Patch: Entered addme")
+
+print("    install mission checker..")
 if(__LISTENING_TO_ADDDOWNLOADABLECONTENT__ == true ) then 
 	print( "Mission Checker: Someone else is already listening...")
 	print("Mission Checker: Exited addme")
@@ -57,6 +59,72 @@ else
 end 
 
 
+print("    install debug mode..")
+
+function uopDebugStartMission()
+	tprint(rema_database)
+	
+	if __uopDebugDatabase__ then
+		rema_database = __uopDebugDatabase__
+	end
+	ScriptCB_SetMissionNames({{Map = __uopDebugMission__, dnldable = nil, Side = 1, SideChar = nil, Team1 = "team1", Team2 = "team2"}}, false)
+	ScriptCB_SetTeamNames(0,0)
+	ScriptCB_EnterMission()
+end
+
+if(gPlatformStr == "PC") then
+	if ScriptCB_IsFileExist("..\\..\\addon\\unofficial_patch\\debug.lua") then
+		require("addon\\unofficial_patch\\debug")
+	end
+end
+
+local uop_debug_backup_push = ScriptCB_PushScreen
+ScriptCB_PushScreen = function(name, ...)
+	if name == "ifs_legal" then
+	
+		if __uopDebugMission__ then	
+			print("")
+			print("----------------------------------------------------------------------------------------------------")
+			print("-- DEBUG MODE -- DEBUG MODE -- DEBUG MODE -- DEBUG MODE -- DEBUG MODE -- DEBUG MODE -- DEBUG MODE --")
+			print("")
+			print("-- mission:", __uopDebugMission__)
+			print("-- profil:", __uopDebugProfile__ or "[NIL]")
+			print("----------------------------------------------------------------------------------------------------")
+			print("")
+
+			if __uopDebugProfile__ then
+			
+				ScriptCB_SetConnectType("wan")
+				ScriptCB_CancelLogin()
+
+				ifs_saveop.doOp = "LoadProfile"
+				ifs_saveop.OnSuccess = function()
+					print("UOP Debug Script: profile loaded")
+					uopDebugStartMission()
+				end
+				ifs_saveop.OnCancel = function() 
+					print("ERROR loading profil..")
+					uopDebugStartMission()
+				end
+				ifs_saveop.profile1 = ScriptCB_tounicode(__uopDebugProfile__)
+				ifs_saveop.profile2 = nil
+				
+				gPrevMixConfig = ScriptCB_GetMixConfig()
+				gPrevEffects   = ScriptCB_EffectsEnabled()
+
+				ifs_movietrans_PushScreen(ifs_saveop)
+			else
+				uopDebugStartMission()
+			end
+			
+			return
+		end
+	end
+	
+	return uop_debug_backup_push(name, unpack(arg))
+
+end
+
 
 __LISTENING_TO_ADDDOWNLOADABLECONTENT__ = true 
-print("Mission Checker: Exited addme")
+print("Unofficial Patch: Exited addme")
